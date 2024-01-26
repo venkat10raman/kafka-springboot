71cadcf8fbff9fd05b7bcaed22c4752648117f1a
venkat10raman <10venkataramana@gmail.com>
2024-01-26 20:33:35 +0530

    added properties

--- src/main/java/com/venkataram/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/venkataram/kafka/consumer/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.venkataram.kafka.consumer;
 
 import static java.lang.String.format;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class KafkaConsumer {
 	
-	@KafkaListener(topics = "quickstart-events", groupId = "myGroup")
+	@Value("${spring.kafka.topic.name}")
+	private String stringTopic;
+	
+	@KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
 	public void consumeMessage(String msg) {
-		log.info(format("Consuming message from quickstart-events Topic :: %s", msg));
+		log.info(format("Consuming message from %s Topic :: %s", stringTopic,  msg));
 	}
 
 }
--- src/main/java/com/venkataram/kafka/producer/KafkaProducer.java
+++ src/main/java/com/venkataram/kafka/producer/KafkaProducer.java
@@ -2,6 +2,7 @@ package com.venkataram.kafka.producer;
 
 import static java.lang.String.format;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,12 @@ public class KafkaProducer {
 	
 	private final KafkaTemplate<String, String> kafkaTemplate;
 	
+	@Value("${spring.kafka.topic.name}")
+	private String stringTopic;
+	
 	public void sendMessage(String msg) {
-		log.info(format("Sending message to quickstart-events Topic :: %s", msg));
-		kafkaTemplate.send("quickstart-events", msg);
+		log.info(format("Sending message to %s Topic :: %s", stringTopic, msg));
+		kafkaTemplate.send(stringTopic, msg);
 	}
 	
 
